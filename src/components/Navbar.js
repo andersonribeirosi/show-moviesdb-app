e3a3fc3a4bb73a702c0b37fcb830a1e1a4405a15
Anderson <andersonribeiro.si@hotmail.com>
2020-06-01 16:52:20 -0300

    Implementing the navbar and adding bootswatch and materializing css

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavbarItem from '../components/NavbarItem'
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+import 'materialize-css/dist/css/materialize.min.css'
+
+const Navbar = () => {
+
+  return (
+    <div className="navbar fixed-top navbar-dark bg-primary">
+      <div className="container">
+        <ul className="navbar-nav">
+          <div className="row">
+            <NavbarItem href="#/home" label="BRISAFilmes" />
+            <NavbarItem href="#/movie" label="Buscar Filmes" />
+          </div>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+export default Navbar
