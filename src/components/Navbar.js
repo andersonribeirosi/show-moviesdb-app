5be19b44507ec5e8d9fd8372a68b8bea374f10c3
Anderson <andersonribeiro.si@hotmail.com>
2020-06-03 23:35:08 -0300

    Adjustments to css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,17 @@ import BrisanetLogo from '../assets/images/brisanet.png'
 const Navbar = () => {
 
   return (
-    <div className="navbar fixed-top navbar-dark">
-      <div className="container">
-        <ul className="navbar-nav">
-          <div className="row">
-            <img id="logoBrisa" src={BrisanetLogo} alt="" /><NavbarItem href="#" label="BRISAFilmes" />
-          </div>
-        </ul>
+    <>
+      <div className="navbar fixed-top navbar-dark">
+        <div className="container">
+          <ul className="navbar-nav">
+            <div className="row">
+              <img id="logoBrisa" src={BrisanetLogo} alt="" /><NavbarItem href="#" label="BRISAFilmes" />
+            </div>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
